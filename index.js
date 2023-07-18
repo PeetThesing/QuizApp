5653db0df8abe042a99aa57fb70e167583902504
Peet Thesing <p.thesing@posteo.de>
2023-07-18 09:01:53 +0200

    fix connecting issues to index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const bodyElement = document.querySelector('[data-js="body"]');
+const cardElement = document.querySelector('[data-js="card"]');
+const addDarkMode = document.querySelector('[data-js="button-dark-mode]');
+
+addDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  cardElement.classList.add("dark");
+});
+
+console.log("Hi");
+
+// darkMode.addEventListener("click", () => {
+//     bodyElement.classList.add("dark");
+//   });
+// function switchDarkmode() {
+//     if (buttonDarkMode = true) {
+
+//     } else
+
+// }
+
+// function myFunction() {
+//     let text = "Text" ;
+//     demoText.textContent = text;
+//   }
