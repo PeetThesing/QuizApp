0e7afec235c587ab70b3350e1fc16e86b58a9e35
Peet Thesing <p.thesing@posteo.de>
2023-07-19 13:51:00 +0200

    add class to svg, home and account works

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const cardElement = document.querySelector('[data-js="card"]');
 const addDarkMode = document.querySelector('[data-js="button-dark-mode"]');
 
-addDarkMode.addEventListener("click", () => {
-  bodyElement.classList.add("dark");
-  cardElement.classList.add("dark");
-});
+// addDarkMode.addEventListener("click", () => {
+//   bodyElement.classList.toggle("dark");
+//   cardElement.classList.toggle("dark");
+// });
 
-console.log("Hi");
+// console.log("Hi");
 
 // darkMode.addEventListener("click", () => {
 //     bodyElement.classList.add("dark");
