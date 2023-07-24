4d03678e443fcf4449b6959923eabef48722d823
Peet Thesing <p.thesing@posteo.de>
2023-07-24 19:33:21 +0200

    fix darkmode

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
+//Elements
 const bodyElement = document.querySelector('[data-js="body"]');
-const cardElement = document.querySelector('[data-js="card"]');
-const addDarkMode = document.querySelector('[data-js="button-dark-mode"]');
 
-// addDarkMode.addEventListener("click", () => {
-//   bodyElement.classList.toggle("dark");
-//   cardElement.classList.toggle("dark");
-// });
+const buttonAddDarkMode = document.querySelector(
+  '[data-js="button-dark-mode"]'
+);
 
-// console.log("Hi");
-
-// darkMode.addEventListener("click", () => {
-//     bodyElement.classList.add("dark");
-//   });
-// function switchDarkmode() {
-//     if (buttonDarkMode = true) {
-
-//     } else
-
-// }
-
-// function myFunction() {
-//     let text = "Text" ;
-//     demoText.textContent = text;
-//   }
+buttonAddDarkMode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
