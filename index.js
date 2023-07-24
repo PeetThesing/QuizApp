a813f95629417d9b729025400748e9e66654cdfe
Peet Thesing <p.thesing@posteo.de>
2023-07-24 20:18:56 +0200

    add toggle svg

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-//Elements
+//Gettings Elements from HTML
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeOn = document.querySelector('[data-js="darkmode-on"]');
+const darkModeOff = document.querySelector('[data-js="darkmode-off"]');
 
 const buttonAddDarkMode = document.querySelector(
   '[data-js="button-dark-mode"]'
 );
 
+//Button for Darkmode
 buttonAddDarkMode.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
+  if (darkModeOn.classList.contains("invisible")) {
+    darkModeOn.classList.remove("invisible");
+    darkModeOff.classList.add("invisible");
+  } else {
+    darkModeOn.classList.add("invisible");
+    darkModeOff.classList.remove("invisible");
+  }
 });
