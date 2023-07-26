7c8ad99708ab3c7e8273c3f4df9eaf2cba5731d7
Peet Thesing <p.thesing@posteo.de>
2023-07-26 09:48:26 +0200

    add create new card.js event, fixed form problems

---
+++ JS/form.js
@@ -0,0 +1,62 @@
+//Gettings Elements from HTML
+const newQuestionForm = document.querySelector('[data-js="form"]');
+const bodyForm = document.querySelector('[data-js="body-form"]');
+
+//Add Eventlistener to Submit-Button
+newQuestionForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data.question);
+  const newCard = document.createElement("section");
+  newCard.innerHTML = `
+<script src="./index.js" defer></script>
+  <section class="card card-new" data-js="card">
+  <button class="card__bookmark" data-js="bookmark">
+    <svg
+      data-js="bookmark-icon"
+      role="img"
+      aria-label="Bookmark this question"
+      xmlns="http://www.w3.org/2000/svg"
+      height="1.5rem"
+      viewBox="0 0 384 512"
+    >
+      <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+      <path
+        fill="#083248"
+        d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z"
+      />
+    </svg>
+    <svg
+      class="invisible"
+      data-js="bookmarked-icon"
+      role="img"
+      aria-label="Bookmarked question"
+      xmlns="http://www.w3.org/2000/svg"
+      height="1.5rem"
+      viewBox="0 0 384 512"
+    >
+      <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+      <path
+        fill="#083248"
+        d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"
+      />
+    </svg>
+  </button>
+  <span>${data.question}</span>
+  <button class="button-card" data-js="show-answer-button">
+    Show answer
+  </button>
+  <button class="button-card invisible" data-js="hide-answer-button">
+    Hide answer
+  </button>
+  <p class="card__answer invisible" data-js="answer">${data.answer}</p>
+  <ul class="card__list">
+    <li class="list__item html">${data.tag}</li>
+  </ul>
+</section>
+  `;
+
+  console.log(newCard.innerHTML);
+  bodyForm.append(newCard);
+});
